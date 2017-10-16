fd5568a46162664ded071502e92bdae4a1974883
venky1014 <adivenky007@gmail.com>
2017-10-16 16:42:13 +0530

    Update convol.m

--- convol.m
+++ convol.m
@@ -1,10 +1,17 @@
 function [hh] = convol(f,g)
 %CONVOL Computes the convolution of f and g
+aa = g;
+
 N = length(f);
 M = length(g);
 
+if N > M
+    aa = [aa zeros(1, N-M)];
+else
+    aa = [aa zeros(1, M-N)];
+end
 
-hh = zeros(1,N);
+hh = zeros(1,N+M-1);
 for i = 1:N
     for j = 1:M
         if i-j > 0
