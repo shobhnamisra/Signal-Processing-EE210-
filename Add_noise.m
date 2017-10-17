65bf0812214e6b21eabc8cd8f939336344972ec4
venky1014 <adivenky007@gmail.com>
2017-10-18 02:15:37 +0530

    Update Add_noise.m

--- Add_noise.m
+++ Add_noise.m
@@ -11,15 +11,34 @@ global h
 
 M = length(fn);
 N = length(ft);
+n = -Tf:Ts:Tf;
 
 %parameters
 w = max(w1,w2);
 Ts = 0.4*pi/w;
 sig = 0.4;
-%r = -.5*ones(M) + (.5+.5)*rand(M,1);
+r = -.5*ones(1,M) + (.5+.5)*rand(1,M);
 s = -.5*ones(1,N) + (.5+.5)*rand(1,N);
 
 f_new = ft + sig*s;
+g_new = fn + sig*r;
 
-plot(-Tf:h:Tf, f_new);
+fig1 = figure;
+plot(-Tf:h:Tf, f_new, 'b');
+
+fig2 = figure;
+plot(n , g_new, 'k.');
+
+plot(n, zeros(1,length(n)), 'k');
+
+ystart = zeros(1, length(n));
+yend = g_new;
+
+
+hold on;
+for i = 1:length(n)
+    plot([n(i); n(i)], [ystart(i); yend(i)], 'k');
+end
+
+
 xlim([-Tf Tf]);
