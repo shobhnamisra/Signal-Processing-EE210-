7b459de7bc7d3f8d7648c1280c4813ed42a56f02
Shobhna Misra <shobhnamisra@Shobhnas-MacBook-Pro.local>
2017-10-18 11:24:14 +0530

    Added simple music processing

--- ImageProc.m
+++ ImageProc.m
@@ -5,7 +5,7 @@ I = imread('random.jpg');
 h = ones(3,3)/9; %filter kernel
 h1 = [ 0 -1 0 ; -1 4 -1 ; 0 -1 0]/4;
 
-I1 = filter_3(I, h0); %calling the filter function
+I1 = filter_3(I, h); %calling the filter function
 
 figure
 imshow(I1/255)
